796658b068cbebc7b9fc3a81187a7f809de6caac
Alex Jiakai Xu <ax2155@columbia.edu>
2023-12-12 22:17:22 -0500

    Add temporary code tryout
    Co-authored-by: Amy Qi <xintongqi@users.noreply.github.com>
    
    This code may not be able to compile. Add here just for note-taking purposes

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,32 @@ module Lib
     ( crossValidation
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (foldl', sortBy)
+import Data.Map (Map, fromListWith, elems)
+import Statistics.Distribution (density)
+import Statistics.Distribution.Normal (normalDistr)
+
 import Utils (calculateMeanAndStdDev, chunk)
+
+-- Function to calculate the probability of a value given the normal distribution
+probability :: Double -> (Double, Double) -> Double
+probability x (mean, stdDev) = density (normalDistr mean stdDev) x
+
+-- Function to predict the label for a given set of features
+predict :: Map Double [(Double, Double)] -> [Double] -> Double
+predict model features =
+  let priorProbabilities = fmap (\distributions -> product $ map (\(f, dist) -> probability f dist) distributions) model
+      posteriorProbabilities = zipWith (*) (elems priorProbabilities) features
+      labelProbabilities = zip (elems priorProbabilities) posteriorProbabilities
+      (label, _) = maximumBy (\(_, p1) (_, p2) -> compare p1 p2) labelProbabilities
+  in label
+
 -- Function to perform cross-validation
 crossValidation :: Int -> [(Double, [Double])] -> Double
+crossValidation k dataset =
+  let chunks = chunk (length dataset `div` k) dataset
+      accuracies = map (\validationSet -> let trainingSet = filter (`notElem` validationSet) dataset
+                                              model = trainNaiveBayes trainingSet
+                                              correctPredictions = length $ filter (\(label, features) -> predict model features == label) validationSet
+                                          in fromIntegral correctPredictions / fromIntegral (length validationSet)) chunks
+  in sum accuracies / fromIntegral k
