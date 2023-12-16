8cb04094b73a31dc33d32528c16d78f397acb6cd
Alex Jiakai Xu <ax2155@columbia.edu>
2023-12-16 00:45:55 -0500

    Tryout sequential method
    This has errors and cannot be built; comment out for now.

---
+++ src/Sequential.hs
@@ -0,0 +1,33 @@
+module Sequential (
+    compareFeature
+) where
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+import Types (Dataset, Model)
+import Utils (calculateMeanAndStdDev, extractFeature, mergeFolds, splitIntoFolds)
+
+-- Function to compare features
+compareFeature :: Dataset -> Int -> Double
+compareFeature dataset k = undefined
+-- compareFeature dataset k =
+--   let indices = [0 .. length (snd (head dataset)) - 1]
+--       accuracies = map (\idx -> splitTest (extractFeature dataset idx) k) indices
+--   in maximum accuracies
+
+-- Function to split test
+splitTest :: Dataset -> Int -> Double
+splitTest dataset k = undefined
+-- splitTest dataset k =
+--   let folds = splitIntoFolds dataset k
+--       accuracies = map (\i -> trainAndEvaluate (mergeFolds (take i folds) (drop (i + 1) folds))) [0..k-1]
+--   in maximumBy (comparing fst) accuracies
+
+-- Function to train and evaluate the model
+trainAndEvaluate :: Dataset -> (Double, Model)
+trainAndEvaluate dataset = undefined
+
+-- Function for making predictions
+prediction :: Model -> Dataset -> Double
+prediction model dataset = undefined
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,9 +1,13 @@
 module Utils (
     calculateMeanAndStdDev,
-    chunk
+    extractFeature,
+    mergeFolds,
+    splitIntoFolds
 ) where
 
-import Data.List (transpose)
+import Data.List.Split (chunksOf)
+
+import Types
 
 -- Function to calculate the mean and standard deviation of a list of values
 calculateMeanAndStdDev :: [Double] -> (Double, Double)
@@ -14,7 +18,17 @@ calculateMeanAndStdDev xs =
       stdDev = sqrt variance
   in (mean, stdDev)
 
--- Function to split a list into chunks for cross-validation
-chunk :: Int -> [a] -> [[a]]
-chunk _ [] = []
-chunk n xs = take n xs : chunk n (drop n xs)
+-- Function to extract a feature from a dataset
+extractFeature :: Dataset -> Int -> Dataset
+extractFeature dataset idx = map (\(label, features) -> (label, [features !! idx])) dataset
+
+-- Function to merge folds for training
+mergeFolds :: [Dataset] -> Dataset
+mergeFolds = concat
+
+-- Function to split dataset into k folds
+splitIntoFolds :: Dataset -> Int -> [Dataset]
+splitIntoFolds dataset k =
+  let (foldSize, remainder) = length dataset `quotRem` k
+      initialFoldSize = foldSize + if remainder > 0 then 1 else 0
+  in chunksOf initialFoldSize dataset
