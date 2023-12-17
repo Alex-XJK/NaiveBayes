0d91275bd75ca88a86dbf6815305d1b1fc0895e9
Amy <xintongqi2@gmail.com>
2023-12-17 12:25:05 -0500

    finish trainModel related

--- src/Sequential.hs
+++ src/Sequential.hs
@@ -1,23 +1,25 @@
-module Sequential (
-    compareFeature
-) where
+module Sequential
+  ( compareFeature,
+  )
+where
 
 import Data.List (maximumBy)
 import Data.Ord (comparing)
-
+import Data.Set qualified as Set
 import Types (Dataset, Model)
-import Utils (calculateMeanAndStdDev, extractFeature, mergeFolds, splitIntoFolds)
+import Utils (calculateLikelihood, calculateMeanAndStdDev, calculateProduct, calculateStats, extractFeature, mergeFolds, splitIntoFolds)
 
 -- Function to compare features
 compareFeature :: Dataset -> Int -> Double
 compareFeature dataset k =
   let indices = [0 .. length (snd (head dataset)) - 1]
       accuracies = map (\idx -> splitTest (extractFeature dataset idx) k) indices
-  in maximum accuracies
+   in maximum accuracies
 
 -- Function to split test
 splitTest :: Dataset -> Int -> Double
 splitTest dataset k = undefined
+
 -- splitTest dataset k =
 --   let folds = splitIntoFolds dataset k
 --       accuracies = map (\i -> trainAndEvaluate (mergeFolds (take i folds) (drop (i + 1) folds))) [0..k-1]
@@ -30,3 +32,18 @@ trainAndEvaluate dataset = undefined
 -- Function for making predictions
 prediction :: Model -> Dataset -> Double
 prediction model dataset = undefined
+
+-- Train a model
+trainModel :: Dataset -> Model
+trainModel samples =
+  let totalSamples = fromIntegral $ length samples
+      labelSet = Set.fromList $ map fst samples
+      labelStats =
+        map
+          ( \label ->
+              let labelSamples = filter ((== label) . fst) samples
+                  labelCount = fromIntegral $ length labelSamples
+               in calculateStats label (map snd labelSamples) (labelCount / totalSamples)
+          )
+          (Set.toList labelSet)
+   in labelStats
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,22 +1,53 @@
-module Utils (
-    calculateMeanAndStdDev,
+module Utils
+  ( calculateMeanAndStdDev,
     extractFeature,
     mergeFolds,
-    splitIntoFolds
-) where
+    splitIntoFolds,
+    calculateLikelihood,
+    calculateProduct,
+    calculateStats,
+  )
+where
 
 import Data.List.Split (chunksOf)
-
 import Types
+
+-- -- Function to calculate the mean and standard deviation of a list of values
+-- calculateMeanAndStdDev :: [Double] -> (Double, Double)
+-- calculateMeanAndStdDev xs =
+--   let n = fromIntegral $ length xs
+--       mean = sum xs / n
+--       variance = sum (map (\x -> (x - mean) ^ 2) xs) / n
+--       stdDev = sqrt variance
+--   in (mean, stdDev)
 
 -- Function to calculate the mean and standard deviation of a list of values
 calculateMeanAndStdDev :: [Double] -> (Double, Double)
 calculateMeanAndStdDev xs =
   let n = fromIntegral $ length xs
       mean = sum xs / n
-      variance = sum (map (\x -> (x - mean) ^ 2) xs) / n
-      stdDev = sqrt variance
-  in (mean, stdDev)
+      variance = sum (map (\x -> (x - mean) ^ 2) xs)
+   in (mean, variance)
+
+-- Function to calculate the statistics for a given label
+calculateStats :: Label -> [Features] -> Double -> LabelStats
+calculateStats label features prior =
+  let labelValues = map (!! 0) features
+      (meanVal, varVal) = calculateMeanAndStdDev labelValues
+   in (label, meanVal, varVal, prior)
+
+-- Calculate the likelihood of x for a normal distribution with mean mu and variance sigma2
+calculateLikelihood :: Double -> Double -> Double -> Double
+calculateLikelihood x mu sigma2 =
+  let exponent = -((x - mu) ** 2) / (2 * sigma2)
+      coefficient = 1 / sqrt (2 * pi * sigma2)
+   in coefficient * exp exponent
+
+-- Calculate the product of likelihood and prior for a given FeatureVector and LabelStats
+calculateProduct :: Features -> LabelStats -> Double
+calculateProduct feature (label, mean, variance, prior) =
+  let likelihood = calculateLikelihood (head feature) mean variance
+   in prior * likelihood
 
 -- Function to extract a feature from a dataset
 extractFeature :: Dataset -> Int -> Dataset
@@ -31,4 +62,4 @@ splitIntoFolds :: Dataset -> Int -> [Dataset]
 splitIntoFolds dataset k =
   let (foldSize, remainder) = length dataset `quotRem` k
       initialFoldSize = foldSize + if remainder > 0 then 1 else 0
-  in chunksOf initialFoldSize dataset
+   in chunksOf initialFoldSize dataset
