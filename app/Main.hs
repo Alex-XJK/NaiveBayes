73e5bc41dc9ef261f7defee8161942232724d81d
Alex Jiakai Xu <ax2155@columbia.edu>
2023-12-18 21:24:51 -0500

    Add command-line argument judge

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,45 @@ module Main (
   main
 ) where
 
-import Generator
-import Sequential (trainBestFeature)
+import System.Environment (getArgs)
+import Generator (generateDataset, generateDatasetParallel)
+import qualified Parallel as Par
+import qualified Sequential as Seq
 
+-- Main function
+-- Run the program with the
+--    "-par" flag to run the parallel functions
+--    "-seq" flag to run the sequential functions
+-- e.g., stack run -- -par
 main :: IO ()
 main = do
+  args <- getArgs
   let totalSize = 100000
-      maxValue = 5
+      labelSize = 5
       featureParams = [(1.0, 0.5), (10.0, 2.0), (10.0, 5.0), (50.0, 10.0), (30.0, 5.0)]
-      gDataset_p = generateDatasetParallel totalSize maxValue featureParams
-  putStrLn "Generated Dataset with Parallel:"
-  print (length gDataset_p)
+      kValue = 4
+  case args of
+    ["-par"] -> runParallelFunctions totalSize labelSize featureParams kValue
+    ["-seq"] -> runSequentialFunctions totalSize labelSize featureParams kValue
+    _        -> putStrLn "Invalid flag. Use either -par or -seq."
 
-  let result = trainBestFeature 4 gDataset_p
+runParallelFunctions :: Int -> Int -> [(Double, Double)] -> Int -> IO ()
+runParallelFunctions totalSize labelSize featureParams kValue = do
+  putStrLn "Running parallel functions..."
+  let dataset = generateDatasetParallel totalSize labelSize featureParams
+  putStr "Generated Dataset: length = "
+  print (length dataset)
+  let result = Par.trainBestFeature kValue dataset
+  putStrLn "Best Feature:"
+  print result
+
+runSequentialFunctions :: Int -> Int -> [(Double, Double)] -> Int -> IO ()
+runSequentialFunctions totalSize labelSize featureParams kValue = do
+  putStrLn "Running sequential functions..."
+  let dataset = generateDataset totalSize labelSize featureParams
+  putStr "Generated Dataset: length = "
+  print (length dataset)
+  let result = Seq.trainBestFeature kValue dataset
   putStrLn "Best Feature:"
   print result
 
