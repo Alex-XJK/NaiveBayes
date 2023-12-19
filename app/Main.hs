0144cdd6d466b390940e4dc7d35c3a4e44ec9aa9
Alex Jiakai Xu <ax2155@columbia.edu>
2023-12-19 18:39:14 -0500

    Support argument for file reading

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,14 @@ module Main (
 ) where
 
 import System.Environment (getArgs)
-import Generator (generateDataset, generateDatasetParallel)
+import Generator (generateDataset, generateDatasetParallel, readDataset)
 import qualified Parallel as Par
 import qualified Sequential as Seq
 
 -- Main function
 -- Run the program with the
---    "-par" flag to run the parallel functions
---    "-seq" flag to run the sequential functions
--- e.g., stack run -- -par
+--    -seq|-par [-file <path>]
+-- e.g., stack run -- -par -file "./data/iris.tsv"
 main :: IO ()
 main = do
   args <- getArgs
@@ -20,9 +19,21 @@ main = do
       featureParams = [(1.0, 0.25, 0.25), (-1.0, 0.5, 0.5), (80.0, 1.2, 0.0), (-2.0, 2.0, 2.0), (2, 1.0, 1.0)] -- [(mean, variance, noise)]
       kValue = 4
   case args of
-    ["-par"] -> runParallelFunctions totalSize labelSize featureParams kValue
-    ["-seq"] -> runSequentialFunctions totalSize labelSize featureParams kValue
-    _        -> putStrLn "Invalid flag. Use either -par or -seq."
+    ["-par", "-file", path] -> runParallelFunctionsWithFile   path                              kValue
+    ["-par"]                -> runParallelFunctions           totalSize labelSize featureParams kValue
+    ["-seq", "-file", path] -> runSequentialFunctionsWithFile path                              kValue
+    ["-seq"]                -> runSequentialFunctions         totalSize labelSize featureParams kValue
+    _                       -> putStrLn "Invalid flag. Usage: -seq|-par [-file <path>]"
+
+runParallelFunctionsWithFile :: String -> Int -> IO ()
+runParallelFunctionsWithFile path kValue = do
+  putStrLn "Running parallel functions with File Reading..."
+  dataset <- readDataset path
+  putStr "Read in Dataset: length = "
+  print (length dataset)
+  let result = Par.trainBestFeature kValue dataset
+  putStrLn "Best Feature:"
+  print result
 
 runParallelFunctions :: Int -> Int -> [(Double, Double, Double)] -> Int -> IO ()
 runParallelFunctions totalSize labelSize featureParams kValue = do
@@ -31,6 +42,16 @@ runParallelFunctions totalSize labelSize featureParams kValue = do
   putStr "Generated Dataset: length = "
   print (length dataset)
   let result = Par.trainBestFeature kValue dataset
+  putStrLn "Best Feature:"
+  print result
+
+runSequentialFunctionsWithFile :: String -> Int -> IO ()
+runSequentialFunctionsWithFile path kValue = do
+  putStrLn "Running sequential functions with File Reading..."
+  dataset <- readDataset path
+  putStr "Read in Dataset: length = "
+  print (length dataset)
+  let result = Seq.trainBestFeature kValue dataset
   putStrLn "Best Feature:"
   print result
 
