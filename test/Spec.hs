4f8729ccab255d0dc154ea3bc13c16e61133c505
Alex Jiakai Xu <ax2155@columbia.edu>
2023-12-20 22:43:44 -0500

    Finish Default Test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,11 @@
-import Generator(sampleDataset, plainDataset, generateDataset, readDataset)
+import Generator (generateDataset)
+import qualified Parallel as Par
 
 main :: IO ()
 main = do
-  let sDataset = sampleDataset
-  putStrLn "Sample Dataset:"
-  print sDataset
-
-  let labels = [0, 1, 0, 2] :: [Int]
-      features1 = [1.0, 2.0, 3.0, 4.0] :: [Double]
-      features2 = [2.0, 3.0, 1.5, 2.5] :: [Double]
-      features3 = [3.0, 4.0, 2.5, 3.5] :: [Double]
-      features4 = [4.0, 5.0, 3.5, 4.5] :: [Double]
-      features5 = [5.0, 6.0, 4.5, 5.5] :: [Double]
-      pDataset = plainDataset labels [features1, features2, features3, features4, features5]
-  putStrLn "Zip 5 features to dataset:"
-  print pDataset
-
-  let totalSize = 5
-      maxValue = 3
-      featureParams = [(1.0, 0.5), (10.0, 2.0), (10.0, 5.0), (50.0, 10.0), (30.0, 5.0)]
-      gDataset = generateDataset totalSize maxValue featureParams
-  putStrLn "Generated Dataset:"
-  print gDataset
-
-  let filePath = "./data/test.tsv"
-  rDataset <- readDataset filePath
-  putStrLn "Read Dataset:"
-  print rDataset
+  let   dataset = generateDataset 100000 5 [(5.0, 0.25), (5.0, 0.25), (5.0, 0.25), (5.0, 0.25), (5.0, 0.25)] [0.25, 0.25, 0, 0.25, 0.25]
+        (model, _) = Par.trainBestFeature 5 dataset
+  putStr "Generator... "
+  putStrLn $ if length dataset == 100000 then "OK" else "FAIL!"
+  putStr "Model... "
+  putStrLn $ if length model == 5 then "OK" else "FAIL!"
